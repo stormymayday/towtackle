66e34b887b4bdc347b222174d097e07207b74ccd
stormymayday <stormymayday7@gmail.com>
2024-12-02 14:31:34 -0800

    tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,22 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    50: "#e6f1ff",
+                    100: "#b3d7ff",
+                    500: "#0066cc",
+                    900: "#003366",
+                },
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
